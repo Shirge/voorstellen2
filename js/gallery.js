7254cd285d100987b75d3ea83ed1c95372e56377
Jeffrey <38684@ma-web.nl>
2025-02-03 21:40:31 +0100

    multiple pages

--- js/gallery.js
+++ js/gallery.js
@@ -25,26 +25,80 @@ class Range {
 let leftButton = document.getElementById("galleryLeftButton");
 let rightButton = document.getElementById("galleryRightButton");
 let imgElements = document.getElementsByClassName("galleryImage");
-let imageGallery = ["img/GarfieldPlanet.png", "img/GarfieldPlanet.png", "img/GarfieldPlanet.png", "img/Pipebomb.jpg", "img/Pipebomb.jpg", "img/Pipebomb.jpg"];
+let imageGallery1 = ["img/GarfieldPlanet.png", "img/GarfieldPlanet.png", "img/GarfieldPlanet.png", "img/Pipebomb.jpg", "img/Pipebomb.jpg", "img/Pipebomb.jpg"];
+let imageGallery2 = ["img/GarfieldPlanet.png", "img/GarfieldPlanet.png", "img/GarfieldPlanet.png", "img/Pipebomb.jpg", "img/Pipebomb.jpg", "img/Pipebomb.jpg"];
+let imageGallery3 = ["img/GarfieldPlanet.png", "img/GarfieldPlanet.png", "img/GarfieldPlanet.png", "img/Pipebomb.jpg", "img/Pipebomb.jpg", "img/Pipebomb.jpg"];
+let imageGallery4 = ["img/GarfieldPlanet.png", "img/GarfieldPlanet.png", "img/GarfieldPlanet.png", "img/Pipebomb.jpg", "img/Pipebomb.jpg", "img/Pipebomb.jpg"];
 let counter = 0;
 let index = new Range([0, 1, 2]);
 
-imgElements[0].src = imageGallery[0];
-imgElements[1].src = imageGallery[1];
-imgElements[2].src = imageGallery[2];
+if (imageGallery1) {
+    imgElements[0].src = imageGallery1[index.value[0]];
+    imgElements[1].src = imageGallery1[index.value[1]];
+    imgElements[2].src = imageGallery1[index.value[2]];
+}
+else if (imageGallery2) {
+    imgElements[0].src = imageGallery2[index.value[0]];
+    imgElements[1].src = imageGallery2[index.value[1]];
+    imgElements[2].src = imageGallery2[index.value[2]];
+}
+else if (imageGallery3) {
+    imgElements[0].src = imageGallery3[index.value[0]];
+    imgElements[1].src = imageGallery3[index.value[1]];
+    imgElements[2].src = imageGallery3[index.value[2]];
+}
+else if (imageGallery4) {
+    imgElements[0].src = imageGallery4[index.value[0]];
+    imgElements[1].src = imageGallery4[index.value[1]];
+    imgElements[2].src = imageGallery4[index.value[2]];
+}
 
 leftButton.onclick = function () {
     index.previous();
 
-    imgElements[0].src = imageGallery[index.value[0]];
-    imgElements[1].src = imageGallery[index.value[1]];
-    imgElements[2].src = imageGallery[index.value[2]];
+    if (imageGallery1) {
+        imgElements[0].src = imageGallery1[index.value[0]];
+        imgElements[1].src = imageGallery1[index.value[1]];
+        imgElements[2].src = imageGallery1[index.value[2]];
+    }
+    else if (imageGallery2) {
+        imgElements[0].src = imageGallery2[index.value[0]];
+        imgElements[1].src = imageGallery2[index.value[1]];
+        imgElements[2].src = imageGallery2[index.value[2]];
+    }
+    else if (imageGallery3) {
+        imgElements[0].src = imageGallery3[index.value[0]];
+        imgElements[1].src = imageGallery3[index.value[1]];
+        imgElements[2].src = imageGallery3[index.value[2]];
+    }
+    else if (imageGallery4) {
+        imgElements[0].src = imageGallery4[index.value[0]];
+        imgElements[1].src = imageGallery4[index.value[1]];
+        imgElements[2].src = imageGallery4[index.value[2]];
+    }
 }
 
 rightButton.onclick = function () {
     index.next();
 
-    imgElements[0].src = imageGallery[index.value[0]];
-    imgElements[1].src = imageGallery[index.value[1]];
-    imgElements[2].src = imageGallery[index.value[2]];
+    if (imageGallery1) {
+        imgElements[0].src = imageGallery1[index.value[0]];
+        imgElements[1].src = imageGallery1[index.value[1]];
+        imgElements[2].src = imageGallery1[index.value[2]];
+    }
+    else if (imageGallery2) {
+        imgElements[0].src = imageGallery2[index.value[0]];
+        imgElements[1].src = imageGallery2[index.value[1]];
+        imgElements[2].src = imageGallery2[index.value[2]];
+    }
+    else if (imageGallery3) {
+        imgElements[0].src = imageGallery3[index.value[0]];
+        imgElements[1].src = imageGallery3[index.value[1]];
+        imgElements[2].src = imageGallery3[index.value[2]];
+    }
+    else if (imageGallery4) {
+        imgElements[0].src = imageGallery4[index.value[0]];
+        imgElements[1].src = imageGallery4[index.value[1]];
+        imgElements[2].src = imageGallery4[index.value[2]];
+    }
 }
